3bc5b59c0a415f167746b1ee22288331923d8061
Tomek <tomasz.kunsztowicz@gmail.com>
2017-03-11 21:45:58 +0100

    Add settings

--- www/js/my-app2.js
+++ www/js/my-app2.js
@@ -1,16 +1,57 @@
+$$(document).on('pageInit', '.page[data-page="about"]', function (e) {
+    // Following code will be executed for page with data-page attribute equal to "about"
+    //myApp.alert('Here comes About page');
+    
+    //set name
+    //set settings
+    //set settings
+    
+    document.getElementById("btn_save").onclick = function fun() { 
+        settings();
+    }
+})
+
+//init variables
 var a, b, c;
+var gender;
+var age;
+var name;
+var units;
 
 function calculate() {
     
     var info;
   
+    /*age = localStorage.getItem('age');
+    name = localStorage.getItem('name');
+    gender = localStorage.getItem('gender');
+    units = localStorage.getItem('units');*/
+    
     a = document.getElementById('height').value;
     b = document.getElementById('weight').value; 
+    
+    
+    if (units == 0) {
+    
     c = b/Math.pow(a/100, 2);
     c = c.toFixed(2);
 
+    } else {
         
-    if (c < 18.50) {
+    c = 703*(b/Math.pow(a/100, 2));
+    c = c.toFixed(2);
+        
+    }
+       
+    
+    if (c < 15) {
+        info = 'Very severely underweight';
+    }
+    else if (c >= 15 && c <= 16) {
+        
+    info = 'Severely nderweight';
+    } 
+    else if (c > 16 && c < 18.5)  {
         info = 'Underweight';
         }
     else if (c >= 18.5 && c <= 24.9) {
@@ -19,8 +60,14 @@ function calculate() {
     else if (c >= 25 && c <= 29.9) {
         info = 'Overweight';
     }
-    else {
-        info = "Obese";
+    else if (c >= 30 && c <= 34.9) {
+        info = 'Moderately obese';
+    }
+    else if (c >= 35 && c <= 39.9) {
+        info = 'Severely obese';
+    }
+    else if (c >= 40) {
+        info = 'Very severely obese';
     }
     
     //validations
@@ -33,13 +80,13 @@ function calculate() {
     myApp.alert("Please give weight");
     }
     
-    if (a <= 150 || a >= 220) {
-        myApp.alert("Please give height in range 150-220cm");
+    if (a <= 50 || a >= 240) {
+        myApp.alert("Please give height in range 50-240cm");
         document.getElementById('height').value = "";
     }
     else
     if (b <= 50 || b >= 150) {
-        myApp.alert("Please give height in range 50-150kg");
+        myApp.alert("Please give height in range 1-150kg");
         document.getElementById('weight').value = ""; 
     }
    
@@ -63,7 +110,66 @@ function reset() {
     document.getElementById('info').value = "";
 }
 
+function settings() {
+    
+    //gender = document.getElementById('h').value;   
+    var radios = document.getElementsByName('gender');
+
+    for (var i = 0, length = radios.length; i < length; i++) {
+        if (radios[i].checked) {
+        // do whatever you want with the checked radio
+        alert(radios[i].value);
+            gender = radios[i].value;
+            //alert(gender);
+        // only one radio can be logically checked, don't check the rest
+        break;
+    }
+    }
+    
+    var radios = document.getElementsByName('units');
+
+    for (var i = 0, length = radios.length; i < length; i++) {
+        if (radios[i].checked) {
+        // do whatever you want with the checked radio
+        alert(radios[i].value);
+            units = radios[i].value;
+            alert(units);
+        // only one radio can be logically checked, don't check the rest
+        break;
+    }
+    }
+    
+    age = document.getElementById('age').value;
+    name = document.getElementById('name').value;
+    
+    //metric / inches
+    //dajemy pomiary z wiki
+    //wersja dla dzieci
+    //cale dolozyc
+    //layout
+    //wypuszczamy
+   
+    localStorage.setItem('age', age);
+    localStorage.setItem('name', name);
+    localStorage.setItem('gender', gender);
+    localStorage.setItem('units', units);
+      
+    //localStorage getItem
+    //age = localStorage.getItem('age');
+    //name = localStorage.getItem('name');
+    //gender = localStorage.getItem('gender');
+    
+    
+}
+
+
 window.onload = function() {
+    
+    age = localStorage.getItem('age');
+    name = localStorage.getItem('name');
+    gender = localStorage.getItem('gender');
+    units = localStorage.getItem('units');
+    
     document.getElementById("btn").onclick = function fun() { 
         calculate();
         //validation code to see State field is mandatory.  
@@ -71,11 +177,18 @@ window.onload = function() {
     document.getElementById("btn_reset").onclick = function fun() { 
         reset();
         //validation code to see State field is mandatory.  
-    }
+    } 
 }
 
 
-    
+
+//TO DO S
+//BUTTONY wycentrowac
+//INCHES LAYOUT zmienia sie w zaleznosci od settingow
+//WYPELNIANIE SETTINGOW AUTOMATYCZNIE
+//WYSWIETLANIE IMIENIA W WYNIKU
+//INFO O WLASCIWYM BMI, porownywanie z wynikiem
+
 
 
 

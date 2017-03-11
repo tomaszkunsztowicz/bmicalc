243ab3cdb5ae07aa4f7febd6e90e8d5c75b116ca
Tomek <tomasz.kunsztowicz@gmail.com>
2017-03-11 10:41:11 +0100

    Changes in layout, settings html add

--- www/js/my-app2.js
+++ www/js/my-app2.js
@@ -1,16 +1,15 @@
+var a, b, c;
+
 function calculate() {
     
-    //sex
+    var info;
+  
     a = document.getElementById('height').value;
     b = document.getElementById('weight').value; 
     c = b/Math.pow(a/100, 2);
-    c = c.toFixed(2)
-    //string
-    var info;
-    //alert(a);
-    //c round to 2 decimal places
-    
-    
+    c = c.toFixed(2);
+
+        
     if (c < 18.50) {
         info = 'Underweight';
         }
@@ -24,27 +23,44 @@ function calculate() {
         info = "Obese";
     }
     
+    //validations
+    
+    if (!a) {
+    myApp.alert("Please give height");
+    }
+   
+    if (!b) {
+    myApp.alert("Please give weight");
+    }
+    
     if (a <= 150 || a >= 220) {
         myApp.alert("Please give height in range 150-220cm");
         document.getElementById('height').value = "";
     }
-    
+    else
     if (b <= 50 || b >= 150) {
         myApp.alert("Please give height in range 50-150kg");
         document.getElementById('weight').value = ""; 
     }
-    
-    
+   
     document.querySelector('#results').innerHTML = c ;
     document.querySelector('#info').innerHTML = info ;
     
-    //validations
     //height weight
     //color of text
-    //rounding of bmi
     //settings metric
-    //man / woman
-    
+    //man / woman  
+}
+
+function reset() {
+    //myApp.alert("Test");
+    a = 0;
+    b = 0;
+    c = 0;
+    document.getElementById('height').value = "";
+    document.getElementById('weight').value = "";
+    document.getElementById('results').value = "";
+    document.getElementById('info').value = "";
 }
 
 window.onload = function() {
@@ -52,4 +68,15 @@ window.onload = function() {
         calculate();
         //validation code to see State field is mandatory.  
     }
+    document.getElementById("btn_reset").onclick = function fun() { 
+        reset();
+        //validation code to see State field is mandatory.  
+    }
 }
+
+
+    
+
+
+
+ 

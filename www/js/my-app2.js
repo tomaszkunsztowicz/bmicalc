1eeb33804b27751f1080dd5372443340511f03d6
Tomek <tomasz.kunsztowicz@gmail.com>
2017-03-13 21:44:07 +0100

    Milestone 1

--- www/js/my-app2.js
+++ www/js/my-app2.js
@@ -1,22 +1,55 @@
-$$(document).on('pageInit', '.page[data-page="about"]', function (e) {
-    // Following code will be executed for page with data-page attribute equal to "about"
-    //myApp.alert('Here comes About page');
-    
-    //set name
-    //set settings
-    //set settings
-    
-    document.getElementById("btn_save").onclick = function fun() { 
-        settings();
-    }
-})
-
 //init variables
 var a, b, c;
 var gender;
 var age;
 var name;
 var units;
+
+$$(document).on('pageInit', '.page[data-page="about"]', function (e) {
+    // Following code will be executed for page with data-page attribute equal to "about"
+    
+    age = localStorage.getItem('age');
+    name = localStorage.getItem('name');
+    gender = localStorage.getItem('gender');
+    units = localStorage.getItem('units');
+    
+    document.getElementById("btn_save").onclick = function fun() { 
+        settings();
+    }
+    
+    var textbox = document.getElementById('name');
+    textbox.value = name;
+    
+    var textbox = document.getElementById('age');
+    textbox.value = age;
+    //document.getElementById('t').value = 'new value';
+
+    
+    var radios = document.getElementsByName('gender');
+    
+    for (var i = 0, length = radios.length; i < length; i++) {
+        if (radios[i].value == gender) {   
+            radios[i].checked = true;
+        // do whatever you want with the checked radio
+        // only one radio can be logically checked, don't check the rest
+        break;
+    }
+    }
+    
+    var radios = document.getElementsByName('units');
+    
+    for (var i = 0, length = radios.length; i < length; i++) {
+        if (radios[i].value == units) {
+            //alert(gender);
+            radios[i].checked = true;
+            //alert("alert");
+        // do whatever you want with the checked radio
+        // only one radio can be logically checked, don't check the rest
+        break;
+    }
+    } 
+})
+
 
 function calculate() {
     
@@ -37,19 +70,17 @@ function calculate() {
     
     a = (a*100)+a_cm;   
     c = b/Math.pow(a/100, 2);
-    c = c.toFixed(2);
-
+        
+    
     } else {
        
     a = (a*12)+a_cm;  
-    alert(a);
-    alert(a_cm);
-    
     c = 703*(b/Math.pow(a, 2));
-    c = c.toFixed(2); 
+        
        
     }
-       
+      
+    
     
     if (c < 15) {
         info = 'Very severely underweight';
@@ -81,38 +112,35 @@ function calculate() {
     if (units == 0) {
     
         if (a <= 50 || a >= 240) {
-            myApp.alert("Please give height in range 50-240cm");
+            myApp.alert("Please give height in range 50-240cm", 'Info');
             document.getElementById('height').value = "";
         }
         else
         if (b <= 50 || b >= 150) {
-            myApp.alert("Please give height in range 1-150kg");
+            myApp.alert("Please give height in range 1-150kg", 'Info');
             document.getElementById('weight').value = ""; 
         }
    
     } else {
         
     if (!a) {
-        myApp.alert("Please give height in feet");
+        myApp.alert("Please give height in feet", 'Info');
         }
 
         if (!a_cm) {
-        myApp.alert("Please give height in inches");
+        myApp.alert("Please give height in inches", 'Info');
         }
 
         if (!b) {
-        myApp.alert("Please give weight");
+        myApp.alert("Please give weight", 'Info');
         }
 
     }
     
-    
-    
-    
-    
-    
-    document.querySelector('#results').innerHTML = c ;
-    document.querySelector('#info').innerHTML = info ;
+    if (c) {
+        document.querySelector('#results').innerHTML = c ;
+        document.querySelector('#info').innerHTML = info ;
+    }
     
     //height weight
     //color of text
@@ -121,28 +149,27 @@ function calculate() {
 }
 
 function reset() {
+    
     //myApp.alert("Test");
     a = 0;
     b = 0;
     c = 0;
-    document.getElementById('height').value = "";
+    a_cm = 0;
+    document.getElementById('height_m').value = "";
+    document.getElementById('height_cm').value = "";
     document.getElementById('weight').value = "";
     document.getElementById('results').value = "";
     document.getElementById('info').value = "";
+    myApp.alert('Values have been reset', 'Info');
 }
 
 function settings() {
     
-    //gender = document.getElementById('h').value;   
     var radios = document.getElementsByName('gender');
 
     for (var i = 0, length = radios.length; i < length; i++) {
-        if (radios[i].checked) {
-        // do whatever you want with the checked radio
-        alert(radios[i].value);
+        if (radios[i].checked) {      
             gender = radios[i].value;
-            //alert(gender);
-        // only one radio can be logically checked, don't check the rest
         break;
     }
     }
@@ -151,11 +178,7 @@ function settings() {
 
     for (var i = 0, length = radios.length; i < length; i++) {
         if (radios[i].checked) {
-        // do whatever you want with the checked radio
-        alert(radios[i].value);
             units = radios[i].value;
-            alert(units);
-        // only one radio can be logically checked, don't check the rest
         break;
     }
     }
@@ -163,22 +186,13 @@ function settings() {
     age = document.getElementById('age').value;
     name = document.getElementById('name').value;
     
-    //metric / inches
-    //cale dolozyc
-    //layout
-    //wypuszczamy
-   
     localStorage.setItem('age', age);
     localStorage.setItem('name', name);
     localStorage.setItem('gender', gender);
     localStorage.setItem('units', units);
-      
-    //localStorage getItem
-    //age = localStorage.getItem('age');
-    //name = localStorage.getItem('name');
-    //gender = localStorage.getItem('gender');
     
-    
+    myApp.alert('Settings saved', 'Info');
+
 }
 
 
@@ -194,7 +208,9 @@ window.onload = function() {
         //validation code to see State field is mandatory.  
     }
     document.getElementById("btn_reset").onclick = function fun() { 
+       
         reset();
+
         //validation code to see State field is mandatory.  
     } 
     
